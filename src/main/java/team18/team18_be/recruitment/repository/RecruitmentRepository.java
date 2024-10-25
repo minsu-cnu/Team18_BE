e0d0066f6f31605abb94db223a0c88908c672b1d
Minji Kim <101392857+alsswl@users.noreply.github.com>
2024-10-25 15:08:28 +0900

    refactor : 코드 정리

--- src/main/java/team18/team18_be/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/team18/team18_be/recruitment/repository/RecruitmentRepository.java
@@ -7,5 +7,6 @@ import team18.team18_be.recruitment.entity.Recruitment;
 import team18.team18_be.userInformation.entity.Company;
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+
   List<Recruitment> findByCompany(Optional<Company> company);
 }
