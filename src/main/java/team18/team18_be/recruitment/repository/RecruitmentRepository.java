f8b6a0918fd0229405872f8ee5eb4edc927673d0
Minji Kim <101392857+alsswl@users.noreply.github.com>
2024-10-25 15:08:01 +0900

    feat : 회사 별 구인글 조회 구현

--- src/main/java/team18/team18_be/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/team18/team18_be/recruitment/repository/RecruitmentRepository.java
@@ -1,10 +1,11 @@
 package team18.team18_be.recruitment.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team18.team18_be.recruitment.entity.Recruitment;
+import team18.team18_be.userInformation.entity.Company;
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
-
-  List<Recruitment> findByCompanyId(Long companyId);
+  List<Recruitment> findByCompany(Optional<Company> company);
 }
