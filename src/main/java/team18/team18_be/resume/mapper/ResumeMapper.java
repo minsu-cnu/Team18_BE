7308c11b6548d9c323e02a70be0eadf2804a56f9
Minji Kim <101392857+alsswl@users.noreply.github.com>
2024-10-31 17:48:00 +0900

    refactor : mapstruct 적용

---
+++ src/main/java/team18/team18_be/resume/mapper/ResumeMapper.java
@@ -0,0 +1,17 @@
+package team18.team18_be.resume.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import team18.team18_be.auth.entity.User;
+import team18.team18_be.resume.dto.request.ResumeRequest;
+import team18.team18_be.resume.dto.response.ResumeResponse;
+import team18.team18_be.resume.entity.Resume;
+
+@Mapper(componentModel = "spring")
+public interface ResumeMapper {
+  ResumeMapper INSTANCE = Mappers.getMapper(ResumeMapper.class);
+  ResumeResponse toResumeResponse(Resume resume);
+
+  Resume toResume(ResumeRequest resumeRequest, User user);
+}
