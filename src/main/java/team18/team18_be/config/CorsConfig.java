93e056d25b277a35a282859eb265ab66ec81cd09
배민수 <duecost@gmail.com>
2024-11-06 16:04:48 +0900

    fix: Configuration 어노테이션 누락된 것 추가

--- src/main/java/team18/team18_be/config/CorsConfig.java
+++ src/main/java/team18/team18_be/config/CorsConfig.java
@@ -1,9 +1,11 @@
 package team18.team18_be.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
   @Override
