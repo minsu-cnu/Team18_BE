90999a80efa11630e59e8e3e85ffeda00eb609a5
배민수 <duecost@gmail.com>
2024-11-06 16:07:06 +0900

    feat: CORS 헤더의 Authorization 필드만 허용하도록 변경

--- src/main/java/team18/team18_be/config/CorsConfig.java
+++ src/main/java/team18/team18_be/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     registry.addMapping("/api/**")
         .allowedOrigins("http://localhost:3000")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
-        .allowedHeaders("*")
+        .allowedHeaders("Authorization")
         .exposedHeaders(HttpHeaders.LOCATION)
         .allowCredentials(true)
         .maxAge(1800);
