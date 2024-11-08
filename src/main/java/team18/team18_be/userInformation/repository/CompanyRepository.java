ea85c46dcb0f59f34d8cca8bf5f36c51a14e0d83
test1 <test>
2024-11-08 15:00:26 +0900

    feat : Optional적용

--- src/main/java/team18/team18_be/userInformation/repository/CompanyRepository.java
+++ src/main/java/team18/team18_be/userInformation/repository/CompanyRepository.java
@@ -1,10 +1,11 @@
 package team18.team18_be.userInformation.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team18.team18_be.auth.entity.User;
 import team18.team18_be.userInformation.entity.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
-  Company findByUser(User user);
+  Optional<Company> findByUser(User user);
 }
