c1036bd4891310647f13e1ba6a416f91623c1967
test1 <test>
2024-11-14 02:03:37 +0900

    feat : 고용주 "게약하기" 문제 해결

--- src/main/java/team18/team18_be/apply/dto/response/ApplierPerRecruitmentResponse.java
+++ src/main/java/team18/team18_be/apply/dto/response/ApplierPerRecruitmentResponse.java
@@ -1,6 +1,6 @@
 package team18.team18_be.apply.dto.response;
 
 public record ApplierPerRecruitmentResponse(Long userId, String name, Long resumeId, Long applyId,
-                                            String applicantNation, String koreanLanguageLevel) {
+                                            String applicantNation, String koreanLanguageLevel, boolean contractExistence) {
 
 }
